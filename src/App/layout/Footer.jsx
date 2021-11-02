db1edfcb929e09ace47bea5f95cf2dd62112fc79
jamie <zomb16jam9@gmail.com>
2021-11-03 00:14:00 +0800

    add footer

---
+++ src/App/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='absolute bottom-0 w-full bg-white text-center py-4'>
+      Taiwan Tourguide © Code: Jamie / Design: KT
+    </footer>
+  )
+}
+
+export default Footer
--- src/App/routes/Home.jsx
+++ src/App/routes/Home.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import Header from '../layout/Header'
+import Footer from '../layout/Footer'
 
 const Home = () => (
-  <Header></Header>
-  //
+  <Fragment>
+    <Header></Header>
+    <div className='h-full pb-16 my-0 mx-auto'></div>
+    <Footer></Footer>
+  </Fragment>
 )
 
 export default Home
